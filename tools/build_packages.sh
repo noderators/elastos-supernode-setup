bebbe1af04a9ac7d2fe4495aba89b2caeace7534
Kiran Pachhai <kiran.pachhai@gmail.com>
2019-11-21 22:15:27 -0500

    Updated elastos-ela to v0.3.9

--- tools/build_packages.sh
+++ tools/build_packages.sh
@@ -30,7 +30,7 @@ function build_deb_package() {
     cd $CURR_DIR
 }
 
-build_deb_package ela elastos-ela 0.3.8-1
+build_deb_package ela elastos-ela 0.3.9-1
 
 build_deb_package did elastos-did 0.1.2-3
 
